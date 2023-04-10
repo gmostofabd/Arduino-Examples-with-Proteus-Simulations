c99e57ed348b1f67527a8837451c4b4d00154bab
Golam Mostofa <78910261+gmostofabd@users.noreply.github.com>
2023-04-10 19:59:49 +0600

    Add files via upload

---
+++ UNO_LED/RELAY_1/RELAY_1.ino
@@ -0,0 +1,17 @@
+#define RELAY  A5                      
+
+void setup()
+{    
+  pinMode(RELAY, OUTPUT);       
+}
+
+void loop()
+{
+  digitalWrite(RELAY,HIGH);           // Turns Relay On
+  delay(500); 
+  digitalWrite(RELAY,LOW);          // Turns Relay Off
+  delay(500); 
+}
+
+
+
