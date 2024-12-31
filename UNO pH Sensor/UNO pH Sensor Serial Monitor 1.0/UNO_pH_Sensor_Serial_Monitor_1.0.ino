385b7e0d7fcb16f9b7776a42ac2d453b63f1271e
Golam Mostofa <mostofa.melab@gmail.com>
2024-12-31 23:04:54 +0600

    Add files via upload

---
+++ UNO pH Sensor/UNO pH Sensor Serial Monitor 1.0/UNO_pH_Sensor_Serial_Monitor_1.0.ino
@@ -0,0 +1,39 @@
+#define SensorPin 0          // the pH meter Analog output is connected with the Arduino’s Analog
+unsigned long int avgValue;  //Store the average value of the sensor feedback
+float b;
+int buf[10],temp;
+ 
+void setup()
+{
+  Serial.begin(9600);  
+  Serial.println("Ready");    //Test the serial monitor
+}
+void loop()
+{
+  for(int i=0;i<10;i++)       //Get 10 sample value from the sensor for smooth the value
+  { 
+    buf[i]=analogRead(SensorPin);
+    delay(10);
+  }
+  for(int i=0;i<9;i++)        //sort the analog from small to large
+  {
+    for(int j=i+1;j<10;j++)
+    {
+      if(buf[i]>buf[j])
+      {
+        temp=buf[i];
+        buf[i]=buf[j];
+        buf[j]=temp;
+      }
+    }
+  }
+  avgValue=0;
+  for(int i=2;i<8;i++)                      //take the average value of 6 center sample
+    avgValue+=buf[i];
+  float phValue=(float)avgValue*5.0/1024/6; //convert the analog into millivolt
+  phValue=3.5*phValue;                      //convert the millivolt into pH value
+  Serial.print("    pH:");  
+  Serial.print(phValue,2);
+  Serial.println(" ");      
+  delay(800);
+}
