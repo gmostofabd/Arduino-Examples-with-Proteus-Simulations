e873c130419562a50f6ed71f63da0ec62563f7f3
Golam Mostofa <mostofa.melab@gmail.com>
2025-01-01 00:06:09 +0600

    Add files via upload

---
+++ UNO DHT11 DHT22 Sensor/UNO_DHT11_DHT22_Sensor_1.0/UNO_DHT11_Sensor_Serial_1.0/DHT_11_with_Arduino.ino
@@ -0,0 +1,36 @@
+#include <SimpleDHT.h>
+
+// for DHT11, 
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT11 = 2;
+SimpleDHT11 dht11(pinDHT11);
+
+void setup() {
+  Serial.println("Temperature and Humidity Data");
+  Serial.begin(9600);
+}
+
+void loop() {
+  // start working...
+
+  delay(250);
+  
+  // read without samples.
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(1000);
+    return;
+  }
+  
+  Serial.print("Sample OK: ");
+  Serial.print((int)temperature); Serial.print(" *C, "); 
+  Serial.print((int)humidity); Serial.println(" H");
+  
+  // DHT11 sampling rate is 1HZ.
+  delay(1500);
+}
